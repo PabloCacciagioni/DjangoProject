7baaeb776bf9ce6ef5f7c6f2a946605ce1693fd8
Pablo Cacciagioni <pablo1234caccia@gmail.com>
2025-09-14 17:54:22 -0300

    1-The-Complete-App-Turns

--- mechanic_workshop/mechanic_workshop/settings.py
+++ mechanic_workshop/mechanic_workshop/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'users',
+    'turns',
+    'vehicles',
+    'mechanics',
+    'spareparts',
+    'services'
 ]
 
 MIDDLEWARE = [
--- mechanic_workshop/turns/models.py
+++ mechanic_workshop/turns/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from vehicles.models import Vehicle
+from services.models import Service
+from spareparts.models import SparePart
 
-# Create your models here.
+class Turn(models.Model):
+    STATES = [
+        ("PENDING", "Pending"),
+        ("CONFIRMED", "Confirmed"),
+        ("FINISHED", "Finished"),
+        ("CANCELED", "Canceled")
+    ]
+    
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, related_name='turns')
+    date = models.DateField()
+    hour = models.TimeField()
+    state = models.CharField(max_length=10, choices=STATES, default="PENDING")
+    description = models.TextField(blank=True, null=True)
+    
+    # Relations with services and spare parts
+    services = models.ManyToManyField(Service, blank=True, related_name='turns')
+    spareparts = models.ManyToManyField(SparePart, blank=True, related_name='turns')
+    
+    create = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f'Turn {self.date} at {self.hour} - {self.vehicle}'
+
+
